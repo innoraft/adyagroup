51aee3760604d4ae7b9f64b401c8e2dd6af804c8
Akash Jain <akash.jain@innoraft.com>
2018-11-27 11:15:53 +0000

    first commit

---
+++ sites/all/themes/ninesixty/custom.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+$('.ui-tabs-nav').prepend('<li class="tabset-tab-2 ui-corner-top first ui-tabs-selected ui-state-active active"><a href="#tabset-tab-2"><span class="tab"><li class="tabset-tab-2 ui-corner-top first ui-tabs-selected ui-state-active active"><a href="#tabset-tab-2"><span class="tab">i</span></a></li></span></a></li>');
+});
